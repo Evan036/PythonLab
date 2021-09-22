8a490bf906e3cf00aa7d18f487493260e6f8fb1e
Evan Huang <evan036@yahoo.com>
2021-09-22 15:09:21 +0800

    any new/

--- ReadRev.py
+++ ReadRev.py
@@ -15,3 +15,15 @@ for d in data:
 	sum_len = sum_len + len(d)
 
 print('每筆留言的平均長度為:',sum_len/len(data))
+
+new = []
+for d in data:
+	if len(d) < 100:
+		new.append(d) #塞選長度小於100的留言清單
+print('一共有', len(new),'筆留言長度小於100')
+
+good = []
+for d in data:
+	if 'good' in d:
+		good.append(d)
+print('一共有', len(good),'筆留言提到good')
